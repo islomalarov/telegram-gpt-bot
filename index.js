55f0bca19a3d099496a7c0a267b52a814088ea0b
islomalarov <islomalarov@gmail.com>
2024-08-30 17:10:47 +0500

    fix: add check conditions

--- index.js
+++ index.js
@@ -16,28 +16,33 @@ bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text && text.toLowerCase().includes('#gpt')) {
-    const query = text.replace(/#gpt/i, '').trim();
+  if (chatId === -1001454021013) {
+    if (text && text.toLowerCase().includes('#gpt')) {
+      const query = text.replace(/#gpt/i, '').trim();
 
-    if (query.length > 0) {
-      try {
-        const chatCompletion = await openai.chat.completions.create({
-          messages: [{ role: "user", content: query }],
-          model: "gpt-3.5-turbo",
-        });
+      if (query.length > 0) {
+        try {
+          const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: query }],
+            model: "gpt-3.5-turbo",
+          });
 
-        const reply = chatCompletion.choices[0].message.content;
-        bot.sendMessage(chatId, reply, { reply_to_message_id: msg.message_id });
-      } catch (error) {
-        if (error.code === 'insufficient_quota') {
-          bot.sendMessage(chatId, 'Извините, достигнут лимит использования API OpenAI. Пожалуйста, попробуйте позже.');
-        } else {
-          bot.sendMessage(chatId, 'Произошла ошибка при обработке запроса.');
+          const reply = chatCompletion.choices[0].message.content;
+          bot.sendMessage(chatId, reply, { reply_to_message_id: msg.message_id });
+        } catch (error) {
+          if (error.code === 'insufficient_quota') {
+            bot.sendMessage(chatId, 'Извините, достигнут лимит использования API OpenAI. Пожалуйста, попробуйте позже.');
+          } else {
+            bot.sendMessage(chatId, 'Произошла ошибка при обработке запроса.');
+          }
+          console.error('Ошибка при взаимодействии с OpenAI:', error);
         }
-        console.error('Ошибка при взаимодействии с OpenAI:', error);
+      } else {
+        bot.sendMessage(chatId, 'Пожалуйста, введите сообщение после хэштега #gpt.');
       }
-    } else {
-      bot.sendMessage(chatId, 'Пожалуйста, введите сообщение после хэштега #gpt.');
     }
+  } else {
+    bot.sendMessage(chatId, 'Не тут-то было')
   }
+
 });
